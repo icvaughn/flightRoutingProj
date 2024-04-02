f4ee3eeb5892cde1bb50997a00d150560378513f
DylanL4556 <drlott@usca.edu>
2024-04-01 20:52:42 -0400

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Airplane {
+public class Airplane {
+    public String make;
+    public String model;
+    public String fuel;
+    public int fuelCapacity;
+    public int fuelConsumption;
+    public int speed;
+    public double range; //unit based on speed unit m or km
+    //this will only be accurate if the units match up (Liters, liters/hr, km/hr or m/hr)
+
+    public Airplane(String make, String model, String fuel, int fuelCapacity, int fuelConsumption, int speed) {
+        this.make = make;
+        this.model = model;
+        this.fuel = fuel;
+        this.fuelCapacity = fuelCapacity;
+        this.fuelConsumption = fuelConsumption;
+        this.speed = speed;
+    }
+    public void setRange(){
+        this.range = (double) fuelCapacity / fuelConsumption * speed;
+    }
 }
--- src/db.java
+++ src/db.java
@@ -1,10 +1,11 @@
 import java.io.FileNotFoundException;
 import java.io.File;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.*;
 import java.lang.*;
-
+import java.io.FileWriter;
+import java.util.HashSet;
+import java.text.DecimalFormat;
 public class db {
     public static File airportDB;
     public static File airplaneDB;
@@ -47,8 +48,11 @@ public class db {
 
                 String[] fts = new String[2];
                 fts[0] = sc.nextLine();
-                nxt = sc.nextLine();
-                if (Objects.equals(nxt, "JA-a")) {
+                if (sc.hasNextLine()) {
+                    nxt = sc.nextLine();
+                }
+
+                if (Objects.equals(nxt, "JA-a") || Objects.equals(nxt, "AVGAS")) {
                     fts[1] = nxt;
                     hasboth = true;
                 } else {
@@ -85,7 +89,48 @@ public class db {
         }
         return aplanes;
     }
+    public static void makeMassiveAportDB(String finalName) {
+        File apdb = new File(finalName);
+        Random randy = new Random();
+        DecimalFormat df = new DecimalFormat("#.##");
+        String[] locations = {"City1,Country1", "City2,Country2", "City3,Country3", "City4,Country4", "City5,Country5", "City6,Country6", "City7,Country7", "City8,Country8", "City9,Country9", "City10,Country10", "City11,Country11", "City12,Country12"};
+        HashSet<String> usedICAOs = new HashSet<>();
+        try (FileWriter writer = new FileWriter(apdb)) {
+            for (int i = 1; i <= 1000; i++) {
+                String ICAO;
+                do {
+                    ICAO = randy.ints(65, 91)
+                            .limit(4)
+                            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                            .toString();
+                } while (usedICAOs.contains(ICAO));
+                usedICAOs.add(ICAO);
 
+                String Loc = locations[randy.nextInt(locations.length)];
+
+                double Long = randy.nextDouble() * 180 * (randy.nextBoolean() ? -1 : 1);
+                double Lat = randy.nextDouble() * 90 * (randy.nextBoolean() ? -1 : 1);
+
+                double freq = randy.nextDouble() * 150;
+
+                String[][] fuelTypes = {{"JA-A", "AVGAS"}, {"JA-A"}, {"AVGAS"}};
+                String[] fts = fuelTypes[randy.nextInt(fuelTypes.length)];
+                Long = Double.parseDouble(df.format(Long));
+                Lat = Double.parseDouble(df.format(Lat));
+                freq = Double.parseDouble(df.format(freq));
+                writer.write(ICAO + "\n");
+                writer.write(Loc + "\n");
+                writer.write(Long + "\n");
+                writer.write(Lat + "\n");
+                writer.write(freq + "\n");
+                for (String ft : fts) {
+                    writer.write(ft + "\n");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("The helpless child laborer could not get the file written: " + e.getMessage());
+        }
+    }
     public void addAirport(airport a) {
         //do data validation when creating airport?
         //update arraylist
@@ -104,9 +149,11 @@ public class db {
     }
 
     public static void main(String[] args) {
-        /*
+        //db dbinst = new db("src/dbDir/airports.txt", "src/dbDir/airplanes.txt");
+        //makeMassiveAportDB("src/dbDir/airports1.txt");
+
             //WORKING CODE TO INTERACT WITH DB: (prints for debug)
-            db dbinst = new db("src/dbDir/airports.txt", "src/dbDir/airplanes.txt");
+            db dbinst = new db("src/dbDir/airports1.txt", "src/dbDir/airplanes.txt");
             ArrayList<airport> aprts = readAirports();
             for (airport a : aprts) {
                 System.out.println(a.CAOid);
@@ -122,6 +169,6 @@ public class db {
             for (Airplane a : aplanes) {
                 System.out.println("Make: " + a.make + " Model: " + a.model + " Fuel: " + a.fuel + " Fuel Capacity: " + a.fuelCapacity + " Fuel Consumption: " + a.fuelConsumption + " Speed: " + a.speed + " Range: " + a.range + " units");
             }
-         */
+
     }
 }
