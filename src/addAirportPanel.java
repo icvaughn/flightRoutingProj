9378062e1e68a5d7e25c485d1b76e25b58f8349b
bcoghi <165708798+bcoghi@users.noreply.github.com>
2024-04-07 03:00:58 -0400

    Update addAirportPanel.java (Functional)
    Functional
    Requires some tweeks

--- src/addAirportPanel.java
+++ src/addAirportPanel.java
@@ -30,46 +30,231 @@ public class addAirportPanel extends JPanel{
 	
 	JButton jbtAdd = new JButton("Add");
 	JPanel panAdd = new JPanel();
+	JLabel lblError = new JLabel("Test");
 	
+	private String airportICAO;
 	private String airportName;
+	private double airportLong;
+	private double airportLat;
+	private double airportFreq;
+	private String[] airportFuel;
+	
+	private db DB = new db("./src/dbDir/airports.txt", "./src/dbDir/airplanes.txt");
 	
 	public addAirportPanel() {
 		setLayout(new GridLayout(7,1,5,5));
+		
 		panIcao.add(lblIcao);
+		ICAOVerifier vIcao = new ICAOVerifier();
+		txtIcao.setInputVerifier(vIcao);
 		panIcao.add(txtIcao);
 		add(panIcao);
 		
 		panName.add(lblName);
+		NameVerifier vName = new NameVerifier();
+		txtName.setInputVerifier(vName);
 		panName.add(txtName);
 		add(panName);
 		
 		panLatLong.add(lblLatLong);
-		panLatLong.add(txtLat);
+		LongVerifier vLong = new LongVerifier();
+		txtLong.setInputVerifier(vLong);
 		panLatLong.add(txtLong);
+		LatVerifier vLat = new LatVerifier();
+		txtLat.setInputVerifier(vLat);
+		panLatLong.add(txtLat);
 		add(panLatLong);
 		
 		panComm.add(lblComm);
+		CommVerifier vComm = new CommVerifier();
+		txtComm.setInputVerifier(vComm);
 		panComm.add(txtComm);
 		add(panComm);
 		
 		panFuel.add(lblFuel);
-		//btnFuel.add(rdFuel1); 
-		//btnFuel.add(rdFuel2);
+		FuelVerifier vFuel1 = new FuelVerifier();
+		rdFuel1.setInputVerifier(vFuel1);
 		panFuel.add(rdFuel1);
+		FuelVerifier vFuel2 = new FuelVerifier();
+		rdFuel2.setInputVerifier(vFuel2);
 		panFuel.add(rdFuel2);
 		add(panFuel);
 		
 		panAdd.add(jbtAdd);
+		lblError.setVisible(false);
+		panAdd.add(lblError);
 		add(panAdd);
 		
 		jbtAdd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println(txtName.getText());
-
-            }
+            	//TODO this shit? While loop gets stuck if you dont select one of the fields first
+            	//But while loop feels better user wise, no need to manually click to another button to refresh error label
+            	//By defualt ,While selecting a text , unable to select another button after add b/c focus will not yield since verify() is false
+            	//EDIT: Scratch on thw while loop I think, but leaving it here in case
+            	System.out.println("add");
+            	//while (true){
+	            	if (vIcao.verify(txtIcao)) {
+	            		if(vName.verify(txtName)) {
+	            			if(vLong.verify(txtLong)) {
+	            				if(vLat.verify(txtLat)) {
+	            					if(vComm.verify(txtComm)) {
+	            						if(vFuel1.verify(rdFuel1)) {
+	            							if(vFuel2.verify(rdFuel2)) {
+	            								System.out.println("Verified");	
+	            								airport nw = new airport(airportICAO, airportName,airportFuel,airportLong,airportLat,airportFreq);
+	            								DB.addAirport(nw); //TODO update this to new db name after initial testing
+	            								DB.readAirports();
+	            								//System.out.println()
+	            								//break;
+	            							}
+	            						}
+	            					}
+	            				}
+	            			}
+	            		}
+	            	}
+            	//}
+            	
+            	
+;            }
         });
 		
 	}
+	class ICAOVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("ICAO Verify");
+			try {
+				airportICAO = txtIcao.getText();
+				//String error = lblError.getText();
+				if(airportICAO.length() == 3 || airportICAO.length() == 4) {
+					if(airportICAO.toUpperCase().equals(airportICAO)) {
+						lblError.setVisible(false);
+						return true;
+					}
+				}
+				lblError.setText("\n ICAO must be 3 or 4 letters and all caps");
+				lblError.setVisible(true);
+				return false;
+			}
+			catch(Exception e) {
+				lblError.setText(e.getMessage());
+				lblError.setVisible(true);
+			}
+			return false;
+		}
 	
-
+	}
+	class NameVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("Name Verify");
+			try {
+				airportName = txtName.getText();
+				if(airportName.matches(".+") && !(airportName.equals("Please enter the Name"))) { //1+ of any char
+					lblError.setVisible(false);
+					return true;
+				}
+				lblError.setText("Name cannot be blank or default text");
+				lblError.setVisible(true);
+				return false;
+			}
+			catch(Exception e) {
+				lblError.setText(e.getMessage());
+				lblError.setVisible(true);
+			}
+			return false;
+		}
+	}
+	class LongVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("Long Verify");
+			try {
+				//System.out.println(txtLong.getText());
+				airportLong  = Double.valueOf(txtLong.getText());
+				if(airportLong >= -180.0 && airportLong <= 180.0) {
+						lblError.setVisible(false);
+						return true;
+					}
+				lblError.setText("Longitude must be within -180 and 180");
+				lblError.setVisible(true);
+				return false;
+				}
+			catch (Exception e) {
+				lblError.setText("Longitude must be a number");
+				lblError.setVisible(true);
+			}
+			return false;
+		}
+	}
+	class LatVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("Lat Verify");
+			try {
+				airportLat = Double.valueOf(txtLat.getText());
+				if(airportLat >= -90.0 && airportLat <= 90.0) {
+					lblError.setVisible(false);
+					return true;
+				}
+				lblError.setText("Latitude must be within -90.0 and 90.0");
+				lblError.setVisible(true);
+				return false;
+			}
+			catch(Exception e) {
+				lblError.setText("Latitude must be a number");
+				lblError.setVisible(true);
+			}
+			return false;
+		}
+	}
+	class CommVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("Comm Verify");
+			try {
+				airportFreq = Double.valueOf(txtComm.getText());
+				if(airportFreq > 0.0) {
+					lblError.setVisible(false);
+					return true;
+				}
+				lblError.setText("Comms Frequency must be > 0");
+				lblError.setVisible(true);
+				return false;
+			}
+			catch(Exception e){
+				lblError.setText("Comms Frequency must be a number");
+				lblError.setVisible(true);
+			}
+			return false;
+		}
+	}
+	class FuelVerifier extends InputVerifier{
+		public boolean verify(JComponent input) {
+			System.out.println("Fuel Verify");
+			try {
+				if(!rdFuel1.isSelected() && !rdFuel2.isSelected()) {
+					lblError.setText("Must select 1 or both fuel types");
+					lblError.setVisible(true);
+					return false;
+				}
+				else if(rdFuel1.isSelected() ^ rdFuel2.isSelected()) {
+					airportFuel = new String[1];
+					if(rdFuel1.isSelected()) {
+						airportFuel[0]="AVGAS";
+					}
+					else {
+						airportFuel[1]="JA-a";
+					}
+				}
+				else if(rdFuel1.isSelected() && rdFuel2.isSelected()) {
+					airportFuel = new String[2];
+					airportFuel[0] = "AVGAS";
+					airportFuel[1] = "JA-a";
+				}
+			}
+			catch(Exception e){
+				lblError.setText(e.getLocalizedMessage());
+				lblError.setVisible(true);
+			}
+			lblError.setVisible(false);
+			return true;
+		}
+	}
 }
